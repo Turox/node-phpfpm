46af8da767e241d7769810adaea3b3e44b6affb5
Adriel Santos <adriel@adriel.eu>
2017-06-16 15:35:03 -0300

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var fastcgiConnector = require('fastcgi-client');
+let fastcgiConnector = require('fastcgi-client');
 module.exports = phpfpm;
-var urlencode = require('urlencode-for-php');
+let urlencode = require('urlencode-for-php');
 
 /**
  * phpfpm
@@ -14,9 +14,16 @@ function phpfpm(options)
 	!options.host && (options.host = '127.0.0.1');
 	!options.port && (options.port = 9000);
 	!options.documentRoot && (options.documentRoot = '');
+	!options.remote_addr && (options.remote_addr = '127.0.0.1');
+	!options.remote_port && (options.remote_port = 1234);
+	!options.server_addr && (options.server_addr = '127.0.0.1');
+	!options.server_port && (options.server_port = 80);
+	!options.server_name && (options.server_name = '127.0.0.1');
+	!options.server_software && (options.server_software = 'Apache 2');
+
 
 	this.options = options;
-	var self = this;
+	let self = this;
 	options.skipCheckServer = true;
 	this.client = fastcgiConnector(options);
 	this.ready = false;
@@ -33,7 +40,7 @@ function phpfpm(options)
  */
 phpfpm.prototype._clearQueue = function()
 {
-	var evt;
+	let evt;
 	while(evt = this.queue.shift())
 	{
 		this.run(evt.info, evt.cb);
@@ -45,7 +52,7 @@ phpfpm.prototype._clearQueue = function()
  */
 phpfpm.prototype.run = function(info, cb)
 {
-	if (typeof info == 'string') info = { method: 'GET', uri: info };
+	if (typeof info === 'string') info = { method: 'GET', uri: info };
 	if (info.url && !info.uri) info.uri = info.url;
 
 	if (!this.ready)
@@ -55,16 +62,16 @@ phpfpm.prototype.run = function(info, cb)
 	}
 
 	//support form data
-	if (info.form && info.method != 'GET')
+	if (info.form && info.method !== 'GET')
 	{
 		info.body = urlencode(info.form);
 		info.method = 'POST';
 	}
 
-	if (info.form && info.method == 'GET')
+	if (info.form && info.method === 'GET')
 	{
 		info.body = '';
-		var qs = urlencode(info.form);;
+		let qs = urlencode(info.form);;
 		info.uri += (info.uri.indexOf('?') === -1) ? '?' + qs : '&' + qs;
 	}
 
@@ -90,17 +97,17 @@ phpfpm.prototype.run = function(info, cb)
 
 	if (info.uri.match(/\?/))
 	{
-		var ms = info.uri.match(/^([^\?]+)\?(.*)$/);
+		let ms = info.uri.match(/^([^\?]+)\?(.*)$/);
 		info.queryString = ms[2];
 		info.uri = ms[1];
 	}
 
-	var phpfile = info.uri;
+	let phpfile = info.uri;
 	if (!phpfile.match(/^\//)) phpfile = this.options.documentRoot + phpfile;
 
 
-	// Default server vars
-	var fastcgiParams = info.fastcgiParams || {
+	// Default server lets
+	let fastcgiParams = info.fastcgiParams || {
 		QUERY_STRING: info.queryString || '',
 		REQUEST_METHOD: info.method,
 		CONTENT_TYPE: info.contentType || '',
@@ -112,17 +119,17 @@ phpfpm.prototype.run = function(info, cb)
 		DOCUMENT_ROOT: this.options.documentRoot,
 		SERVER_PROTOCOL: 'HTTP/1.1',
 		GATEWAY_INTERFACE: 'CGI/1.1',
-		REMOTE_ADDR: '127.0.0.1',
-		REMOTE_PORT: 1234,
-		SERVER_ADDR: '127.0.0.1',
-		SERVER_PORT: 80,
-		SERVER_NAME: '127.0.0.1',
-		SERVER_SOFTWARE: 'node-phpfpm',
+		REMOTE_ADDR: this.options.remote_addr,
+		REMOTE_PORT: this.options.remote_port,
+		SERVER_ADDR: this.options.server_addr,
+		SERVER_PORT: this.options.server_port,
+		SERVER_NAME: this.options.server_name,
+		SERVER_SOFTWARE: this.options.server_software,
 		REDIRECT_STATUS: 200
 	};
 
 
-	var self = this;
+	let self = this;
 
 	self.client.request(fastcgiParams, function(err, request)
 	{
@@ -132,7 +139,7 @@ phpfpm.prototype.run = function(info, cb)
 			return;
 		}
 
-		var body = '',errors = '';
+		let body = '',errors = '';
 		request.stdout.on('data', function(data)
 		{
 			body += data.toString('utf8');
@@ -149,7 +156,7 @@ phpfpm.prototype.run = function(info, cb)
 			cb(false, body, errors);
 		});
 
-		if (info.method == 'POST')
+		if (info.method === 'POST')
 		{
 			request.stdin._write(info.body, 'utf8');
 		}
---
+++ test/test_$_server.php
@@ -0,0 +1,2 @@
+<?php
+echo 'remote_addr '.$_SERVER['REMOTE_ADDR'].', remote_port: '.$_SERVER['REMOTE_PORT'];
